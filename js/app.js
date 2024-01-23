e2bb2b984889637d56c2d9ff64a3e832ff2c0d23
Rati <sifrashvilirati@gmail.com>
2024-01-23 12:12:12 +0300

    js commit

---
+++ js/app.js
@@ -0,0 +1,86 @@
+// 1) დაწერეთ კოდი სადაც შეამოწმებთ დადებითია თუ უარყოფითი 
+// ეს რიცხვი!!
+
+//asnwer: 
+
+
+//2)დაწერეთ მარტივი კალკულატორი სადაც გამოიყენებთ ოპერატორებს(+, -, *, /)
+// switch case დახმარებით უბრალოდ დარწმუნდით რომ თითოეულ ქეისი ასრულებს ამ მეთოდებს 
+
+//asnwer: 
+
+//3) დაწერეთ იგივე კალკულატორი if else დახმარებით!!
+
+//asnwer: 
+
+//4) დაწერეთ ავტორიზაციის ლოგიკა სადაც უნდა დავარეგისტრიროთ User
+// user უნდა შემოყავდეს firstName,lastName,email,password,phoneNumber
+// firstName,lastName,email,password არის სავალდებულო ველები
+// ამიტომ ერთიანად შეამოწმეთ ეს ოთხი ველი და დარწმუნდით თუ ცარიელი input 
+// არარის მაშინ დალოგეთ რომ შენ დარეგისტრირდი წარმატებით თუ ცარიელია მაშინ
+// დალოგეთ "input is required" გამოიყენეთ if else
+
+
+
+
+//1)
+
+//  let Number= "-44"
+
+//  if  (  Number >0) {console.log(positive)}else {console.log(negative)}
+
+
+
+//2)
+
+// let input = "-"
+
+// switch (input) {
+//     case "-":
+//         console.log(7-5)
+//         break;
+
+//     case "+":
+//         console.log(7+5)
+//         break;
+
+//     case "*":
+//         console.log(7*5)
+        
+//     case "/":
+//         console.log(7/5) ;
+
+// }
+
+
+
+
+//3)
+
+//  let input = "-"
+//   if (input= "-") {
+
+//     console.log(45-1)
+    
+// }
+
+// else if (input= "+") {
+//     console.log(43+1)
+// } 
+// else if (input="*") {
+//     console.log(44*1)
+// } 
+
+
+// else  { 
+
+//     console.log(44/1)
+
+
+// }
+
+
+
+
+    
+
